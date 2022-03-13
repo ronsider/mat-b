2bceec7fb364e9f9cdbffbd932c9df235f873f5b
ronsider <ronsider2@gmail.com>
2022-03-13 13:19:24 +0200

    add header and implemntation files

---
+++ mat.cpp
@@ -0,0 +1,6 @@
+#include "mat.hpp"
+
+std::string ariel::mat(int c,int r,char s1,char s2)
+{
+    return "";
+}
---
+++ mat.hpp
@@ -0,0 +1,6 @@
+#include <string>
+
+namespace ariel
+{
+    std::string mat(int c,int r,char s1,char s2);
+}//namespace ariel
