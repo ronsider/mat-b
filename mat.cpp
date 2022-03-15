019f167751018a2ad577fb477a6b7da273ee2ff9
ronsider <ronsider2@gmail.com>
2022-03-15 16:20:53 +0200

    main.cpp is ommited

--- mat.cpp
+++ mat.cpp
@@ -3,13 +3,14 @@
 
 using std::vector;
 using std::pair;
+using std::string;
 
 void fill_mat(vector<vector<char>>& vv,const pair<int,int>& p1,
                                         const pair<int,int>& p2,const char& s)
 {
-   for(int i=p1.first;i<=p2.first;i++)
+   for(int i=p1.first;i<p2.first;i++)
      {
-       for(int j=p1.second;j<=p2.second;j++)
+       for(int j=p1.second;j<p2.second;j++)
          {
            vv[i][j]=s;
          }
@@ -43,12 +44,34 @@ vector<vector<char>> area(int c,int r,char sym1,char sym2)
   return vs;
 }
 
+string mat_to_str(const vector<vector<char>>& vv)
+{
+  string s{};
+  for(const auto& i:vv)
+    {
+      for(const auto& j:i)
+        {
+          s+=j;
+        }
+      s+='\n';
+    }
+  return s;
+}
+
 
 std::string ariel::mat(int c,int r,char s1,char s2)
 {
-  if((c%2==0 || r%2==0) && (c<1 || r<1) )
+  if((c%2==0 || r%2==0) )
   {
       throw "dimensions do not satisfy conditions";
   }
+  if (c<1 || r<1)
+  {
+    throw "dimensions do not satisfy conditions";
+  }
+  
+  vector<vector<char>>vt = area(c,r,s1,s2);
+  std::string s_r = mat_to_str(vt);
+  return s_r;
 
 }
